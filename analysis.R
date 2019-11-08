66380ee5a3e999c3d731cd9b184ed9d4135a4ac5
Valentina Pescuma <valentinapescuma@ua-visitor-ten-21-139-101.uniaccess.unimelb.edu.au>
2019-11-08 16:16:51 +1100

    commit

--- analysis.R
+++ analysis.R
@@ -1,9 +1,19 @@
-# analysis script for project
+# analysis script for project ####
 
-# read data
+# read data ####
+# apples <- data.frame(apple_id = as.factor(1:1000),
+#                      weight = rnorm(1000, 5, 1))
+# write.csv(apples, 'data/apples_data.csv', row.names = F)
 
-# process data
+apples <- read.csv('data/apples_data.csv')
 
-# graphing
+# process data ####
+apples$log_weight <- log(apples$weight)
 
-# print image
+# graphing ####
+hist(apples$weight)
+
+# print image ####
+png('figs/hist_apple_weight.png')
+hist(apples$weight)
+dev.off()
